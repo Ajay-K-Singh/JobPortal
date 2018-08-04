1cf574f30c6238346c9f5bd709b5254233aa798c
Sammi-93 <aksingh1493@gmail.com>
2018-08-05 02:02:53 +0530

    Adding database and server

---
+++ job-portal/backend/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+
+const app = express();
+
+mongoose
+    .connect(
+        'mongodb+srv://ajay:ajay1493@job-portal-ziech.mongodb.net/test?retryWrites=true', { useNewUrlParser: true }
+    )
+    .then(() => {
+        console.log("Connected to database!");
+    })
+    .catch(() => {
+        console.log("Connection failed!");
+    });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    next();
+});
+
+module.exports = app;
