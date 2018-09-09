9857dc1b1b2918973d95f92698f2c72e368d717b
Sammi-93 <aksingh1493@gmail.com>
2018-09-09 22:17:25 +0530

    ada

--- job-portal/src/app/auth/register/register.component.ts
+++ job-portal/src/app/auth/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { CustomErrorStateMatcher } from '../../material/error-state-matcher';
 @Component({
   selector: 'register-form',
   templateUrl: './register.component.html',
@@ -11,16 +11,30 @@ export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   @Output() private formReady : EventEmitter<FormGroup> = new EventEmitter<FormGroup>();
   @Output() onSignUp: EventEmitter<FormGroup> = new EventEmitter<FormGroup>();
+  matcher = new CustomErrorStateMatcher();
+  passwordNotSame: boolean = false;
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm =  formBuilder.group({
+      emailOrUserName: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['']
+    }, { 
+      validator:  this.checkPasswords
+    })
+  }
   ngOnInit() {
-    this.registerForm = new FormGroup({
-      'emailOrUserName': new FormControl(null),
-      'password': new FormControl(null),
-    });
+  }
+
+  checkPasswords(group: FormGroup) {
+    let password = group.controls.password.value;
+    let confirmPassword = group.controls.confirmPassword.value;
+    return password === confirmPassword ? null : { notSame: true }
   }
 
   onSignupClick() {
     if (this.registerForm.valid) {
       this.onSignUp.emit(this.registerForm);
+      this.registerForm.reset();
     }
   }
 }
