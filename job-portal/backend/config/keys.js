9ed06a6de5081c8b7bf2c7948163fd6adc0198b6
Sammi-93 <aksingh1493@gmail.com>
2018-09-24 23:44:23 +0530

    rem

---
+++ job-portal/backend/config/keys.js
@@ -0,0 +1,22 @@
+module.exports = {
+  google: {
+    clientID: '63925814284-0vn0kuluka31fj6j5vgqbgfgck4vgpdo.apps.googleusercontent.com',
+    clientSecret: 'QccuX9YSId4w1wM40pGHiPGk',
+    callbackURL: '/auth/google/callback'
+  },
+  facebook: {
+    clientID: '1877249495686432',
+    clientSecret: '030fad80fc58a66d9adf56b9e23fea18',
+    callbackURL: '/auth/facebook/callback'
+  },
+  linkedin: {
+    clientID: '81uyw430q8yxj0',
+    clientSecret: '8foDLpHPi7T4okFE',
+    callbackURL: '/auth/linkedin/callback'
+  },
+  github: {
+    clientID: 'e7d481e3bab95dacc8fd',
+    clientSecret: '471dc90702daa6eef829f10a75526f17fc0e0e77',
+    callbackURL: '/auth/github/callback'
+  }
+}
